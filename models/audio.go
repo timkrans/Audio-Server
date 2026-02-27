52d5c65d8eba4c569cced990e83f57766ddbafbd
timkrans <tkransberger@yahoo.com>
2026-02-27 13:16:08 -0500

    Adding the model for audio

--- models/audio.go
+++ models/audio.go
@@ -1,1 +1,14 @@
 package models
+
+package models
+
+type Audio struct {
+    ID                 uint   `json:"id" gorm:"primaryKey"`
+    Title              string `json:"title"`
+    AudioFilePath      string `json:"video_file_path"`
+    CoverImageFilePath string `json:"cover_image_file_path"`
+}
+
+type AudioInput struct {
+    Title string `form:"title" binding:"required"`
+}
