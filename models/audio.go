21574d7f6a00cdbcb2ccde0bbd135e20a6fb01bc
timkrans <tkransberger@yahoo.com>
2026-02-28 00:15:45 -0500

    updating the models for the hls filepath

--- models/audio.go
+++ models/audio.go
@@ -1,10 +1,11 @@
 package models
 
 type Audio struct {
-    ID                 uint   `json:"id" gorm:"primaryKey"`
-    Title              string `json:"title"`
-    AudioFilePath      string `json:"video_file_path"`
-    CoverImageFilePath string `json:"cover_image_file_path"`
+	ID                 uint   `json:"id" gorm:"primaryKey"`
+	Title              string `json:"title"`
+	AudioFilePath      string `json:"audio_file_path"`
+	CoverImageFilePath string `json:"cover_image_file_path"`
+	HLSPlaylistPath    string `json:"hls_playlist_path"`
 }
 
 type AudioInput struct {
